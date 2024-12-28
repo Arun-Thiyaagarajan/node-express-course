756088ad68870bc7feb0887d73a388a3c6be95ac
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2024-12-28 11:27:32 +0530

    Till 242: Completed Project

--- 06.5-jobster-api/starter/controllers/jobs.js
+++ 06.5-jobster-api/starter/controllers/jobs.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
 import Job from '../models/Job.js';
 import Errors from '../errors/index.js';
+import pkg from 'mongoose';
+import moment from 'moment';
 
 const { BadRequestError, NotFoundError } = Errors;
 
@@ -125,10 +127,55 @@ const deleteJob = async (req, res) => {
     res.status(StatusCodes.OK).send();
 }
 
+const showStats = async (req, res) => { 
+
+    let stats = await Job.aggregate([
+        { $match: { createdBy: pkg.Types.ObjectId(req.user.userId) } },
+        { $group: { _id: '$status', count: { $sum: 1 } } }
+    ]);
+
+    stats = stats.reduce((acc, curr) => {
+        const { _id: title, count } = curr;
+        acc[title] = count;
+        return acc;
+    }, {});
+
+    const defaultStats = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        declined: stats.declined || 0,
+    };
+
+    let monthlyApplications = await Job.aggregate([
+        { $match: { createdBy: pkg.Types.ObjectId(req.user.userId) } },
+        {
+            $group: {
+                _id: { month: { $month: '$createdAt' }, year: { $year: '$createdAt' } },
+                count: { $sum: 1 }
+            }
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 },
+    ]);
+
+    monthlyApplications = monthlyApplications.map(item => {
+        const { _id: { year, month }, count } = item;
+        const date = moment().month(month - 1).year(year).format('MMM Y');
+        return { date, count };
+    });
+
+    console.log(monthlyApplications)
+
+    res.status(StatusCodes.OK).json({
+        defaultStats, monthlyApplications
+    });
+}
+
 export {
     getAllJobs,
     getJob,
     createJob,
     updateJob,
     deleteJob,
+    showStats,
 }
--- 06.5-jobster-api/starter/routes/jobs.js
+++ 06.5-jobster-api/starter/routes/jobs.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAllJobs, getJob, createJob, updateJob, deleteJob } from '../controllers/jobs.js'
+import { getAllJobs, getJob, createJob, updateJob, deleteJob, showStats } from '../controllers/jobs.js'
 import testUser from '../middleware/testUser.js';
 
 const router = new Router();
 
 router.route('/').post(testUser, createJob).get(getAllJobs);
+router.route('/stats').get(showStats);
 router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob);
 
 export default router;
