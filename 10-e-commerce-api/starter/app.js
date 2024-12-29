7fb139a003cc4c4ef51b7b2b4ec4631becc13d01
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2024-12-29 22:22:16 +0530

    Connect to DB

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,15 +1,21 @@
 import express, { json } from "express";
+import { config } from "dotenv";
 import { connectDB } from "./db/connect.js";
 
+// .env Configuration
+config();
+// express.js Configuration
 const app = express();
 
+// Middleware
 app.use(json());
 
 const port = process.env.PORT || 5001;
 
 const start = async () => {
     try {
-        // await connectDB()
+        await connectDB(process.env.MONGO_URI);
+        console.log('Database connection established!')
         app.listen(port, () =>
             console.log(`Server Listening on http://localhost:${port}`)
         );
