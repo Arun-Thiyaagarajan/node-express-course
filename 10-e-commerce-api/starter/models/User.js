61978878e1f59980d55dccc36cb0fc07e819ca5a
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2024-12-29 23:07:50 +0530

    User Model Updated

---
+++ 10-e-commerce-api/starter/models/User.js
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+
+
+const UserScehma = Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide a email address'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email'
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    }
+});
+
+export default model('User', UserScehma);
