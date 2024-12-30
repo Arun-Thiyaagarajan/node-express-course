f9b97776043490bc5aa9a75e5953c44a64aa1fcc
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2024-12-30 20:29:48 +0530

    Logout Route

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -38,7 +38,11 @@ const login = async (req, res) => {
 }
 
 const logout = async (req, res) => {
-    res.send('logout user');
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now())
+    });
+    res.status(StatusCodes.NO_CONTENT).send();
 }
 
 export {
