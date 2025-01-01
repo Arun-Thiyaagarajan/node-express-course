89ab4e7720a5e8bc865f23ffe6552ce110562b06
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2025-01-01 14:59:11 +0530

    Populate Method

--- 10-e-commerce-api/starter/controllers/reviewController.js
+++ 10-e-commerce-api/starter/controllers/reviewController.js
@@ -28,7 +28,10 @@ const createReview = async (req, res) => {
 }
 
 const getAllReviews = async (req, res) => {
-    const reviews = await Review.find({});
+    const reviews = await Review.find({}).populate({
+        path: 'product',
+        select: 'name company price'
+    });
     res.status(StatusCodes.OK).json({ reviews, totalCount: reviews.length });
 }
 
