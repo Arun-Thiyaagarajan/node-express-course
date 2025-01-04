cdaff1fc31fd8b25b276f30d34695a49242c52f2
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2025-01-04 11:52:00 +0530

    Verify Email Controller

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -26,6 +26,21 @@ const register = async (req, res) => {
     verificationToken: user.verificationToken,
   });
 };
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || user.verificationToken !== verificationToken) {
+    throw new UnauthenticatedError('Verification Failed');
+  }
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = '';
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: `${email} is verified successfully!` });
+}
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -61,4 +76,5 @@ export {
   register,
   login,
   logout,
+  verifyEmail,
 };
